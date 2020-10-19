9b145a31769290daa292cf1a051e2eb246624266
User <User@DESKTOP-6N6EHH6>
2020-10-19 22:54:55 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan9/teks/PBOIF210119067Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067;
+
+/**
+ *
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119067 {
+ /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
